4b47a959bd0b9de60254bcb0343cfae49ed4d9d7
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-03-19 16:56:16 +0000

    Create a function to create a series of new HTML elements

--- wainwrights_start_code/app.js
+++ wainwrights_start_code/app.js
@@ -5,12 +5,29 @@ const getAllWainwrights = async () => {
     const response = await fetch('https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json')
     const wainwrightsData = await response.json();
 
-    
+    wainwrightsData.forEach((response) => {
+        const wainwrightName = document.createElement("li");
+        wainwrightName.innerText = response.name;
+        list.appendChild(wainwrightName);
+
+        const wainwrightHeight = document.createElement("p");
+        wainwrightHeight.innerText = response.heightMetres;
+        wainwrightName.appendChild(wainwrightHeight);
+
+        const wainwrightLatitude = document.createElement("p");
+        wainwrightLatitude.innerText = response.latitude;
+        wainwrightName.appendChild(wainwrightLatitude);
+
+        list.appendChild(wainwrightName);
+    });
 }
 
 
 
+// const getAllButton = document.querySelector("#get-all");
+// getAllButton.addEventListener("click", getAllWainwrights);
 
+// getAllWainwrights();
 
 
 
